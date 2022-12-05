8eb727a0760f9dbd3378a6ca03084b2d8d9eb11f
Uli B <ulja.bybljuk@gmail.com>
2022-12-05 17:37:45 -0500

    FizzBuzz exercise

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+function fizzBuzz(input) {
+    if (typeof(input) !== "number") {
+        return "Not a Number");
+    }
+    if (input % 3 === 0) {
+        return "Fizz";
+    }
+    if (input % 5 === 0 && input % 3 === 0) {
+        return "FizzBuzz";
+    }
+    if (input % 5 === 0) {
+        return "Buzz";
+    }
+    return input;
+}
+
+const output = fizzBuzz(7);
+console.log(output);
