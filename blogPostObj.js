51acc50e99621832343341910ab433f96f18edae
Uli B <ulja.bybljuk@gmail.com>
2023-01-27 15:36:55 -0500

    litteral creating blog post object

---
+++ blogPostObj.js
@@ -0,0 +1,18 @@
+let blogPost = {
+    title: 'The Title',
+    body: 'This is a body.',
+    author: 'Username',
+    views: 555,
+    comments: [
+        {
+            author: 'username1',
+            body: 'this is a commentBody1'
+        },
+        {
+            author: 'username2',
+            body: 'this is a commentBody2'
+        }
+    ],
+    isLive: false
+}
+console.log(blogPost);
