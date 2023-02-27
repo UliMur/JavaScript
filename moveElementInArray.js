b3f967306842e985726ca968c6fedd8e38a7787f
Uli B <ulja.bybljuk@gmail.com>
2023-02-27 14:24:15 -0500

    move element in array

---
+++ moveElementInArray.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4];
+
+const output = move(numbers, 0, 1);
+
+console.log(output);
+
+function move(array, index, offset) {
+    const newIndex = index + offset;
+    if (newIndex < 0 || newIndex >= array.length) {
+        console.error('Invalid offset');
+        return;
+    } 
+    const arrayCopy = [...array];
+    const element = arrayCopy.splice(index, 1)[0];
+    arrayCopy.splice(newIndex, 0, element);
+
+    return arrayCopy;
+}
